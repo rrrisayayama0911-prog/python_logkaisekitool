ffd01f3310b6dd78c8aa82723b3f142125a41f8c
risaoya <rrrisayayama0911@gmail.com>
2026-04-26 08:32:58 +0900

    ERROR抽出（表示）みためstrip()で改行消す

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 #pythonログ解析ツール
 
-#4/25:ERROR抽出（表示）、countもする
+#4/26:ERROR抽出（表示）、countもする
 count_ERROR=0
 count_INFO=0
 count_START=0
 
 with open("log.txt","r",encoding="utf-8")as f:
 
+    #lineに改行含まれるからstrip()で改行消す
     for line in f:
         if "ERROR" in line:
-
-            print(f"ERROR該当箇所：{line}")
+            print(f"ERROR該当箇所：{line.strip()}")
             count_ERROR +=1
-            print(f"ERROR回数：{count_ERROR}\n")
         
-        if "INFO" in line:
-            
-            print(f"INFO該当箇所：{line}")            
+        if "INFO" in line:           
+            print(f"INFO該当箇所：{line.strip()}")            
             count_INFO+=1
-            print(f"INFOの回数：{count_INFO}\n")
             
-        if "START" in line:
             
-            print(f"START該当箇所：{line}")          
+        if "START" in line:         
+            print(f"START該当箇所：{line.strip()}")          
             count_START+=1
-            print(f"STARTの回数：{count_START}\n")
+            
+    print("------------")
+            
+    print(f"ERROR回数：{count_ERROR}")
+    print(f"INFOの回数：{count_INFO}")
+    print(f"STARTの回数：{count_START}")
+    
+    print("------------")
 
     
