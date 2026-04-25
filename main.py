21d0c616efe24b7ec6a47c3e76cd35bbe71679b3
risaoya <rrrisayayama0911@gmail.com>
2026-04-26 08:06:41 +0900

    ERROR抽出（表示）出力修正

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #pythonログ解析ツール
 
-#4/25:ERROR抽出（表示）
-
+#4/25:ERROR抽出（表示）、countもする
 count=0
-
 with open("log.txt","r",encoding="utf-8")as f:
-    
-    for line in f:
 
+    for line in f:
         if "ERROR" in line:
-            print(line)
+
+            print(f"ERROR該当箇所：{line}")
 
             count +=1
-print(count)
+            print(f"ERROR回数：{count}")
+
+    
