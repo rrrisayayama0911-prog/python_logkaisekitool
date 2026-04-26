4a2d715664bf17d33a7c7fddc5c74e0f002f48b0
risaoya <rrrisayayama0911@gmail.com>
2026-04-26 20:04:55 +0900

    #4/26:ERROR抽出（表示）、count結果を記載した書き込みファイルを作成を関数化

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 #pythonログ解析ツール
 
-#4/26:ERROR抽出（表示）、count結果を記載した書き込みファイルを作成
-count_ERROR=0
-count_INFO=0
-count_START=0
+#4/26:ERROR抽出（表示）、count結果を記載した書き込みファイルを作成を関数化
 
-#書き込み用の変数はout
-with open("output.txt","w",encoding="utf-8")as out:
-    #読み込み用の変数はf
-    with open("log.txt","r",encoding="utf-8")as f:
-
-        #lineに改行含まれるからstrip()で改行消す
-        for line in f:
-            if "ERROR" in line:
-                out.write(f"ERROR該当箇所：{line.strip()}\n")
-                count_ERROR +=1
-
-            if "INFO" in line:           
-                out.write(f"INFO該当箇所：{line.strip()}\n")            
-                count_INFO+=1
+#関数で処理をまとめる
+def analyze_log(in_f,out_f,keywords): #keywordはcui時点では使用しない
+    
+    count_ERROR=0
+    count_INFO=0
+    count_START=0
 
 
-            if "START" in line:         
-                out.write(f"START該当箇所：{line.strip()}\n")          
-                count_START+=1
+    #書き込み用の変数はout_f
+    with open("output.txt","w",encoding="utf-8")as out_f:
+        #読み込み用の変数はin_f
+        with open("log.txt","r",encoding="utf-8")as in_f:
 
-        out.write("------------\n")
+            #lineに改行含まれるからstrip()で改行消す
+            for line in in_f:
+                if "ERROR" in line:
+                    #書き込みファイルの()の内容を文字列として書き込む
+                    out_f.write(f"ERROR該当箇所：{line.strip()}\n")
+                    count_ERROR +=1
 
-        out.write(f"ERROR回数：{count_ERROR}\n")
-        out.write(f"INFOの回数：{count_INFO}\n")
-        out.write(f"STARTの回数：{count_START}\n")
+                if "INFO" in line:
+                    #書き込みファイルの()の内容を文字列として書き込む           
+                    out_f.write(f"INFO該当箇所：{line.strip()}\n")            
+                    count_INFO+=1
 
-        out.write("------------")
+
+                if "START" in line:
+                    #書き込みファイルの()の内容を文字列として書き込む         
+                    out_f.write(f"START該当箇所：{line.strip()}\n")          
+                    count_START+=1
+
+            out_f.write("------------\n")
+
+            out_f.write(f"ERROR回数：{count_ERROR}\n")
+            out_f.write(f"INFOの回数：{count_INFO}\n")
+            out_f.write(f"STARTの回数：{count_START}\n")
+
+            out_f.write("------------")
+
+
+#1回だけ呼ぶ
+keywords =["ERROR","INFO","START"]
+analyze_log("log.txt","output.txt",keywords)
+    
     
 
     
